22bf17c02d6d034a41f06cf7f9e4552cba049534
Frank <FrankBoesing@users.noreply.github.com>
2026-09-20 20:59:08 +0200

    Revert "Unitial upload"
    This reverts commit 62f3638abfbac66eb3afdf38a46965ce0403b287.

--- Dusk2Dawn.h
+++
@@ -1,8 +0,0 @@
-/*  Dusk2Dawn.h
- *  Get time of sunrise and sunset.
- *  Created by DM Kishi <dm.kishi@gmail.com> on 2017-02-01.
- *  Updated to modern C++ for microcontrollers by Frank B.on 2026-09-20
- *  <https://github.com/FrankBoesing/Dusk2Dawn++>
- */
-
-#include "Dusk2Dawn.hpp"
